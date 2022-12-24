ebeba55678e0d650f3d159c141847156a69613cd
Glen-Can-Roche <glenroche777@gmail.com>
2022-12-24 12:04:34 +0530

    day03 done

--- day-03/code3.cpp
+++ day-03/code3.cpp
@@ -1,2 +1,22 @@
 #include<iostream.h>
+#include<iomanip.h>
+#include<conio.h>
 
+int main()
+{
+    int i, n, num[50], a=0, b;
+
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    for(i=0; i<n; i++) {
+        cin>>num[i];
+        a = a + num[i];
+    }
+    b = a/n;
+    for(i=0; i<n; i++) {
+        if(num[i]>b) {
+            cout<<num[i]<<"  ";
+        }
+    }
+    return 0;
+}
